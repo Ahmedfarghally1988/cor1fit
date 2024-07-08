a9d24c7f71eabcf33055550f2ea4b4074e30d3c6
Ahmed Ahmed Farghally <ahmed.fr1988@gmail.com>
2024-07-09 02:22:40 +0300

    edit-home

---
+++ js/script.js
@@ -0,0 +1,35 @@
+$(window).scroll(function(){
+    if ($(this).scrollTop() > 50) {
+        $('.header-sticky').addClass('newClass');
+    } else {
+        $('.header-sticky').removeClass('newClass');
+    }
+});
+
+const swiperEl = document.querySelector('swiper-container')
+Object.assign(swiperEl, {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+        clickable: true,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 5,
+            spaceBetween: 50,
+        },
+    },
+});
+swiperEl.initialize();
