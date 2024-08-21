9ba58fd76e79d66df226e01d8440e1686454293f
Ahmed Ahmed Farghally <ahmed.fr1988@gmail.com>
2024-08-22 02:42:43 +0300

    f

--- js/script.js
+++ js/script.js
@@ -174,3 +174,31 @@ $(document).ready(function() {
         $('#overlay').modal('hide');
     }, 5000);
 });
+
+
+$(document).ready(function() {
+    $('button').click(function(){
+        var btntxt = $(this).text();
+
+        var copy = $(this).parent().find('.copy').text();
+
+        var $temp = $("<input>");
+        $("body").append($temp);
+        $temp.val(copy).select();
+        document.execCommand("copy");
+        $temp.remove();
+
+
+        $('.confirmation').hide().html(' تم تسح كود الخصم الخاص بك ').fadeIn(100).delay(800).fadeOut(200);
+
+        $( '.main' ).trigger( "click" );
+    });
+
+    $('.main div').click(function(){
+        var range = document.createRange();
+        var selection = window.getSelection();
+        range.selectNodeContents(this);
+        selection.removeAllRanges();
+        selection.addRange(range);
+    });
+});
