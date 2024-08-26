0a6d5f9de92165ede9789b728ad5e1d5fe14c833
Ahmed Ahmed Farghally <ahmed.fr1988@gmail.com>
2024-08-26 21:07:52 +0300

    f

--- js/script.js
+++ js/script.js
@@ -72,7 +72,7 @@ $('.team_work .owl-carousel').owlCarousel({
     dots: true,
     responsive:{
         0:{
-            items:1
+            items:2
         },
         600:{
             items:3
